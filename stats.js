4fb76160b4dbaac2980b3b2ca497719c2ca3dd14
jbiers <juliabiersuriano@gmail.com>
2022-03-31 16:46:20 -0400

    finished stats.js

--- stats.js
+++ stats.js
@@ -1,9 +1,13 @@
 const os = require('os')
 
-const { freemem, totalmem } = os
+setInterval(() => {
+    const { freemem, totalmem } = os
 
-const freeMB = parseInt(freemem() / 1024 / 1024);
-const totalMB = parseInt(totalmem() / 1024 / 1014);
-const percentUsage = ((totalMB - freeMB) * 100) / totalMB;
+    const freeMB = parseInt(freemem() / 1024 / 1024);
+    const totalMB = parseInt(totalmem() / 1024 / 1014);
+    const percentUsage = ((totalMB - freeMB) * 100) / totalMB;
 
-console.table({ Free: freeMB, Total: totalMB, Usage: percentUsage });
+    console.clear()
+    console.log('====Computer Memory====')
+    console.table({ Free: `${freeMB} MB`, Total: `${totalMB} MB`, Usage: `${percentUsage.toFixed(2)}%` });
+}, 1000);
